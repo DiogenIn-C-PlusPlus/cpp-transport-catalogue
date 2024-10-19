b756fc43bac7f7033e929e43541a44df765ae11e
DiogenIn-C-PlusPlus <158086559+DiogenIn-C-PlusPlus@users.noreply.github.com>
2024-10-19 07:51:47 +0300

    first commit stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,29 @@
-// место для вашего кода
+#pragma once
+
+#include <algorithm>
+#include <iomanip>
+#include <iosfwd>
+#include <iostream>
+#include <string_view>
+#include <unordered_set>
+
+#include "geo.h"
+#include "transport_catalogue.h"
+
+namespace detail
+{
+class StatReader
+{
+public:
+    void ParseAndAddRequest(std::string_view request);
+    void PrintResults(const Catalogue::TransportCatalogue& transport_catalogue, std::ostream& output) const;
+private:
+    void PrintFindStops(const Catalogue::TransportCatalogue& transport_catalogue, std::ostream& output, std::string_view request) const;
+    void PrintFindBuses(const Catalogue::TransportCatalogue& transport_catalogue, std::ostream& output, std::string_view request) const;
+    double DistanceInRoute(const Catalogue::TransportCatalogue& transport_catalogue, std::string_view request) const;
+    size_t ComputeUniqStops(const Catalogue::TransportCatalogue& transport_catalogue, std::string_view request) const;
+    void PrintOutBus(std::string_view name_bus, size_t count_stops, size_t uniq_stops, double distance, std::ostream& output, size_t number_out) const;
+    void PrintOutStop(const std::set<std::string_view> number_buses, std::string_view name_stop, std::ostream& output, size_t number_operation) const;
+    std::deque<std::string> save_requests_;
+};
+}
