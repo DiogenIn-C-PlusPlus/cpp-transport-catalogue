610422c65c7d29b08e9843e322c90f5e71c0f278
Mr.Diogen <158086559+DiogenIn-C-PlusPlus@users.noreply.github.com>
2024-12-29 16:43:48 +0300

    Add method AddBus transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -6,35 +6,17 @@ void Catalogue::TransportCatalogue::AddStop(const std::string& name_stop, Coordi
     check_stop_[stops_.back().name_stop_] = &stops_.back();
 }
 
-void Catalogue::TransportCatalogue::AddDistanceForBus(std::vector<DistanceWithStops> name_stop_and_dist_next_stop)
+void Catalogue::TransportCatalogue::AddBus(std::string name_bus, const std::vector<std::string_view>& route, double distance)
 {
-        for(const DistanceWithStops& temp: name_stop_and_dist_next_stop)
-        {
-            for(auto name_stop_and_dist: temp.finish_stop_and_dist_)
-            {
-                std::string_view start_stop = check_stop_[temp.start_name_stop_]->name_stop_;
-                std::string_view finish_stop = check_stop_[name_stop_and_dist.name_stop_]->name_stop_;
-                std::pair<std::string_view,std::string_view> key = std::make_pair(start_stop, finish_stop);
-                start_stop_to_finish_stop_and_dist_between_[key] = name_stop_and_dist.distance_to_next_;
-            }
-        }
-}
-
-void Catalogue::TransportCatalogue::AddBuses(std::vector<BusIncludeNameStops> names_and_routes, std::vector<Catalogue::TransportCatalogue::DistanceWithStops> name_stop_and_dist_next_stop)
-{
-    for(const auto& element: names_and_routes)
-    {
-     Bus bus;
-     bus.name_bus_ = element.name_bus_;
-    for(std::string_view stop: element.route_)
-      {
-        bus.stops_in_route_.push_back(check_stop_[stop]);
-      }
-    buses_.push_back(std::move(bus));
-    AddStopIncludeOtherRoutes(buses_.back().stops_in_route_, buses_.back().name_bus_);
-    check_bus_[buses_.back().name_bus_] = &buses_.back();
-    }
-    AddDistanceForBus(std::move(name_stop_and_dist_next_stop));
+     std::vector<Stop*> stops;
+     for(std::string_view name_stop: route)
+     {
+         stops.push_back(check_stop_[name_stop]);
+     }
+     buses_.push_back(Bus{name_bus, stops});
+     check_bus_[buses_.back().name_bus_] = &buses_.back();
+     AddStopIncludeOtherRoutes(buses_.back().stops_in_route_, buses_.back().name_bus_);
+     bus_name_and_dist_[buses_.back().name_bus_] = distance;
 }
 
 const Catalogue::TransportCatalogue::Stop* Catalogue::TransportCatalogue::FindStop(std::string_view name_stop) const
@@ -65,14 +47,14 @@ void Catalogue::TransportCatalogue::AddStopIncludeOtherRoutes(const std::vector<
     }
 }
 
-std::set<std::string_view> Catalogue::TransportCatalogue::GetBusesEnterInRoute(const Stop* stop) const
+const std::unique_ptr<std::set<std::string_view>> Catalogue::TransportCatalogue::GetBusesEnterInRoute(const Stop* stop) const
 {
     auto temp = stop_enter_in_routes_.find(stop);
     if(temp == stop_enter_in_routes_.end())
     {
-        return {};
+        return nullptr;
     }
-    return temp->second;
+    return std::make_unique<std::set<std::string_view>>(temp->second);
 }
 
 double Catalogue::TransportCatalogue::DistanceInRouteGeo(std::string_view request) const
@@ -85,34 +67,6 @@ double Catalogue::TransportCatalogue::DistanceInRouteGeo(std::string_view reques
     return dist_in_route;
 }
 
-size_t Catalogue::TransportCatalogue::GetDistanceBetweenStops(std::string_view start, std::string_view finish) const // Если не было указано расстояний, то x == y и y == x, поэтому проводим зеркальный поиск
-{
-    std::pair<std::string_view, std::string_view> first_check = std::make_pair(start,finish);
-    std::pair<std::string_view, std::string_view> second_check = std::make_pair(finish, start);
-    auto key_one = start_stop_to_finish_stop_and_dist_between_.find(first_check);
-    auto key_second = start_stop_to_finish_stop_and_dist_between_.find(second_check);
-
-    if(key_one != start_stop_to_finish_stop_and_dist_between_.end())
-    {
-        return start_stop_to_finish_stop_and_dist_between_.at(first_check);
-    }
-    else if(key_second != start_stop_to_finish_stop_and_dist_between_.end())
-    {
-        return start_stop_to_finish_stop_and_dist_between_.at(second_check);
-    }
-    return 0;
-}
-
-size_t Catalogue::TransportCatalogue::DistanceInRouteFromRequests(std::string_view request) const
-{
-    size_t result_dist = 0;
-    for(size_t i = 0; i + 1 <  FindBus(request)->stops_in_route_.size(); ++i)
-    {
-        result_dist += GetDistanceBetweenStops(FindBus(request)->stops_in_route_[i]->name_stop_, FindBus(request)->stops_in_route_[i + 1]->name_stop_);
-    }
-    return result_dist;
-}
-
 size_t Catalogue::TransportCatalogue::ComputeUniqStops(std::string_view request) const
 {
     std::unordered_set<std::string> result;
@@ -123,16 +77,15 @@ size_t Catalogue::TransportCatalogue::ComputeUniqStops(std::string_view request)
     return result.size();
 }
 
-
 Catalogue::TransportCatalogue::BusStatistics Catalogue::TransportCatalogue::GetBusStatistics(std::string_view request) const
-{  
+{
         if(FindBus(request) == nullptr)
         {
             return BusStatistics{request,0,0,0,0};
         }
         size_t count_stops = FindBus(request)->stops_in_route_.size();
         size_t uniq_stops = ComputeUniqStops(request);
-        double distance = DistanceInRouteFromRequests(request);
+        double distance = bus_name_and_dist_.at(request);
         double curvature = distance / DistanceInRouteGeo(request);
         return BusStatistics{request,count_stops, uniq_stops, distance, curvature};
 }
