906746d2b0201e02f9f7c190e41a3c5be949b3a7
Mr.Diogen <158086559+DiogenIn-C-PlusPlus@users.noreply.github.com>
2024-12-29 16:44:17 +0300

    Remove AddBuses transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -3,6 +3,7 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <memory>
 #include <unordered_map>
 #include <unordered_set>
 
@@ -11,21 +12,36 @@
 
 namespace Catalogue
 {
-//struct ConnectStopDist; // Для приватной области, чтобы можно было использовать словарь
+                                                     // Подскажите пожалуйста, что ещё можно улучшить в проекте? //
+
+// Если у Вас есть время можете пожалуйста ответить на пару вопросов
+//   1) Я реализовал деструктор для типа Bus 93 row in this file, but при окончании проекта он вызывается и видимо удаляться нечему и программа падает, 
+// как отследить, что остается в памяти как мусор на текущий момент и понять почему в самом конце вызывает деструктор, хотя по областям видимости он должен
+// был удалиться раньше?
+
+//  2) Меня беспокоит, что я не использовал string_view в input_reader.cpp 149 row, хряняться string хоть и временно, но неприятно согласитесь, происходит инвалидация string_view в одном моменте
+//  159 row file input_reader.cpp в образование пары у элемента element.id, хотя сдесь указатель ссылается на вектор commands_, который уже получается не меняется, по идее релокации нет и он находиться в классе
+// все должно быть хорошо, но он падает...?
 
 
+// Благодарю за уделенное время! :)
+
+// Если не получиться снова встретится до этого года, то с Наступающим!)
 class TransportCatalogue
 {
 private:
 
     struct Stop
     {
-        Stop() = default;
+        Stop() = delete;
         explicit Stop(const std::string& name_stop, const Coordinates& coordinates)
             :name_stop_(name_stop), coordinates_(coordinates)
         {}
-        Stop(Stop&& other)
+        explicit Stop(const Stop& other)
             : name_stop_(other.name_stop_), coordinates_(other.coordinates_)
+        {}
+        explicit Stop(Stop&& other) noexcept
+            : name_stop_(std::move(other.name_stop_)), coordinates_(std::move(other.coordinates_))
         {}
         std::string name_stop_;
         Coordinates coordinates_;
@@ -33,18 +49,68 @@ private:
 
     struct Bus
     {
+        Bus() = delete;
+
+        explicit Bus(Bus&& other) noexcept
+            : name_bus_(std::move(other.name_bus_)), stops_in_route_(std::move(other.stops_in_route_))
+        {
+            other.stops_in_route_.clear();
+        }
+        explicit Bus(const std::string& name_bus, const std::vector<Stop*>& stops_in_route)
+            : name_bus_(name_bus), stops_in_route_(stops_in_route)
+        {}
+
+        explicit Bus(const Bus& other)
+            : name_bus_(other.name_bus_)
+        {
+            stops_in_route_.reserve(other.stops_in_route_.size());
+            for(const auto* stop : other.stops_in_route_)
+            {
+                stops_in_route_.push_back(new Stop(*stop));
+            }
+        }
+
+        Bus& operator=(Bus&& other) noexcept
+        {
+            if(this != &other)
+            {
+                name_bus_ = std::move(other.name_bus_);
+                stops_in_route_ = std::move(other.stops_in_route_);
+                other.stops_in_route_.clear();
+            }
+            return *this;
+        }
+
+        Bus& operator=(const Bus& other)
+        {
+           if(this != &other)
+           {
+               name_bus_ = other.name_bus_;
+               for(Stop* stop: stops_in_route_)
+               {
+                   delete stop;
+               }
+               stops_in_route_.clear();
+
+               stops_in_route_.reserve(other.stops_in_route_.size());
+               for(Stop* stop: other.stops_in_route_)
+               {
+                   stops_in_route_.push_back(new Stop(*stop));
+               }
+           }
+           return *this;
+        }
+
+        ~Bus() // Возникает ошибка
+        {
+//            for(Stop* stop: stops_in_route_)
+//            {
+//                delete  stop;
+//            }
+        }
+
         std::string name_bus_;
         std::vector<Stop*> stops_in_route_;
-    };
-
-    struct HasherDistStop
-    {
-       size_t operator()(const std::pair<std::string_view,std::string_view>& start_finish) const
-       {
-                size_t start = std::hash<std::string_view>{}(start_finish.first);
-                size_t finish = std::hash<std::string_view>{}(start_finish.second);
-                return start + 37 * finish;
-        }
     };
 
     std::deque<Stop> stops_;
@@ -53,11 +119,9 @@ private:
     std::unordered_map<std::string_view, Bus*> check_bus_;
     std::unordered_map<std::string_view, double> lenght_in_bus_;
     std::unordered_map<const Stop*,std::set<std::string_view>> stop_enter_in_routes_; // можно сделать string прибавим в скорости (наверно)
-    std::unordered_map<const std::pair<std::string_view,std::string_view>, size_t, HasherDistStop> start_stop_to_finish_stop_and_dist_between_;
+    std::unordered_map<std::string_view, double> bus_name_and_dist_;
     void AddStopIncludeOtherRoutes(const std::vector<Stop *>& stops, std::string_view name_bus); // Добавил передачу по ссылке
     double DistanceInRouteGeo(std::string_view request) const;
-    size_t DistanceInRouteFromRequests(std::string_view request) const;
-    size_t GetDistanceBetweenStops(std::string_view start, std::string_view finish) const;
     size_t ComputeUniqStops(std::string_view request) const;
 
 public:
@@ -68,30 +132,72 @@ public:
         explicit FinishStopDist(const std::string_view name_stop , const double distance_to_next) // string -> string_view
             : name_stop_(name_stop) ,distance_to_next_(distance_to_next)
         {}
+
+        bool operator==(const FinishStopDist& other) const
+        {
+            return name_stop_ == other.name_stop_ && distance_to_next_ == other.distance_to_next_;
+        }
+
+        bool operator!=(const FinishStopDist& other) const
+        {
+            return !(*this == other);
+        }
+
        std::string_view name_stop_;
        double distance_to_next_;
     };
 
     struct DistanceWithStops
     {
-          explicit DistanceWithStops(DistanceWithStops&& other)
-            : start_name_stop_(other.start_name_stop_), finish_stop_and_dist_(other.finish_stop_and_dist_)
-        {}
+          explicit DistanceWithStops(DistanceWithStops&& other) noexcept
+            : start_name_stop_(std::move(other.start_name_stop_)), finish_stop_and_dist_(std::move(other.finish_stop_and_dist_))
+          {}
           explicit DistanceWithStops(const std::string& name_stop, const std::vector<FinishStopDist>& stop_dist) // string -> string_view
               : start_name_stop_(static_cast<std::string>(name_stop)), finish_stop_and_dist_(std::move(stop_dist))
           {}
+
+            DistanceWithStops(const DistanceWithStops& other)
+                : start_name_stop_(other.start_name_stop_), finish_stop_and_dist_(other.finish_stop_and_dist_)
+            {}
+
+           bool operator==(const DistanceWithStops& other) const
+           {
+               return start_name_stop_ == other.start_name_stop_ && finish_stop_and_dist_ == other.finish_stop_and_dist_;
+           }
+
+           bool operator!=(const DistanceWithStops& other) const
+           {
+               return !(*this == other);
+           }
+
           std::string start_name_stop_;
           std::vector<FinishStopDist> finish_stop_and_dist_;
     };
 
     struct BusIncludeNameStops
     {
-        explicit BusIncludeNameStops(BusIncludeNameStops&& other)
+        explicit BusIncludeNameStops(BusIncludeNameStops&& other) noexcept
+       : name_bus_(std::move(other.name_bus_)), route_(std::move(other.route_))
+        {}
+
+        explicit BusIncludeNameStops(std::string name_bus, std::vector<std::string_view> route) // Возможно лучше переделать const std::string& на string_view, объект все равно новый создасться (как в busstatistics
+       : name_bus_(std::move(name_bus)), route_(std::move(route))
+        {}
+
+        BusIncludeNameStops (const BusIncludeNameStops& other)
        : name_bus_(other.name_bus_), route_(other.route_)
         {}
-        explicit BusIncludeNameStops(const std::string& name_bus,const std::vector<std::string_view>& route) // Возможно лучше переделать const std::string& на string_view, объект все равно новый создасться (как в busstatistics
-            : name_bus_(name_bus), route_(std::move(route))
-        {}  
+
+        bool operator==(const BusIncludeNameStops& other) const
+        {
+            return name_bus_ == other.name_bus_ && route_ == other.route_;
+        }
+
+        bool operator!=(const BusIncludeNameStops& other) const
+        {
+            return !(*this == other);
+        }
+
         std::string name_bus_;
         std::vector<std::string_view> route_;
     };
@@ -114,12 +220,9 @@ public:
     BusStatistics GetBusStatistics(std::string_view request) const;
     void AddStop(const std::string& name_stop, Coordinates coordinates);
     const Stop* FindStop(std::string_view name_stop) const;
-    void AddBuses(std::vector<BusIncludeNameStops> names_and_routes, std::vector<DistanceWithStops> name_stop_and_dist_next_stop);
+    void AddBus(std::string name_bus, const std::vector<std::string_view>& route, double distance);
     const Bus* FindBus(std::string_view name_bus) const;
-    std::set<std::string_view> GetBusesEnterInRoute(const Stop* stop) const;
-
-private: // Как лучше сделать? Проблема если объявить выше в private будет конфликт, что нет объявления, а если объявить выше будет конфликт, что части находятся в private и public одновременно
-       void AddDistanceForBus(std::vector<DistanceWithStops> name_stop_and_dist_next_stop);
+    const std::unique_ptr<std::set<std::string_view>> GetBusesEnterInRoute(const Stop* stop) const; // Хотел через optional ссылку, потом подумал более громоздко решил через умный указатель - это хуже или лучше?
 };
 
 }
