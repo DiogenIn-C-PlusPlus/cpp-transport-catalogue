9bc1be953fcb7f10538570b5fdc06a6af6397933
Mr.Diogen <158086559+DiogenIn-C-PlusPlus@users.noreply.github.com>
2025-04-22 15:17:56 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include <deque>
 #include <set>
 #include <string>
@@ -26,19 +26,20 @@ private:
     std::unordered_map<const Stop*,std::set<std::string>> stop_enter_in_routes_; // можно сделать string прибавим в скорости, единственно мы в памяти проигрываем
     std::unordered_map<std::string_view, double> bus_name_and_dist_;
     void AddStopIncludeOtherRoutes(const Stop* stop, const std::string& name_bus); // Добавил передачу по ссылке
-
-public:
-
-    const std::unique_ptr<std::unordered_map<std::string, std::pair<bool, bool>>> GetRoudtripRoute() const;
-    void SetRoudtripRoute(std::pair<std::string, bool> name_answer, bool first_last_stop);
-    double GetDistanceFromRequests(std::string_view request) const;
     double DistanceInRouteGeo(std::string_view request) const;
     size_t ComputeUniqStops(std::string_view request) const;
+    double GetDistanceFromRequests(std::string_view request) const;
+    const std::unique_ptr<std::set<std::string>> GetBusesEnterInRoute(const Stop* stop) const;
+public:
+
+    const std::unique_ptr<std::set<std::string>> GetBusesByStop(const std::string_view& stop_name) const;
+    std::optional<BusStatistics> GetBusStatistics(const std::string_view& request) const;
+    const std::unique_ptr<std::unordered_map<std::string, std::pair<bool, bool>>> GetRoudtripRoute() const;
+    void SetRoudtripRoute(std::pair<std::string, bool> name_answer, bool first_last_stop);
     void AddStop(const std::string& name_stop, Coordinates coordinates);
     const Stop* FindStop(std::string_view name_stop) const;
     void AddBus(std::string name_bus, const std::vector<std::string> &route, double distance);
     const Bus* FindBus(std::string_view name_bus) const;
-    const std::unique_ptr<std::set<std::string>> GetBusesEnterInRoute(const Stop* stop) const; // Хотел через optional ссылку, потом подумал более громоздко решил через умный указатель - это хуже или лучше?
 };
 
 }
